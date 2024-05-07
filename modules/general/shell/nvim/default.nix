059b7107c8bdf5470c05e296890460661aee53af
Shinsaber <shinsaber@shincraft.fr>
2024-05-07 15:07:53 +0200

    Wip

--- modules/general/shell/nvim/default.nix
+++ modules/general/shell/nvim/default.nix
@@ -1,82 +1,20 @@
-{ config, lib, ... }:
+{ config, lib, nixvim, system, nix-unstable, ... }:
 let
   cfg = config.shincraft.shell.nvim;
+
+  nixvim' = nixvim.legacyPackages.${system};
+  nixvimModule = {
+    pkgs = nix-unstable.legacyPackages.${system};
+    module = import ./nvim.nix; # import the module directly
+    # You can use `extraSpecialArgs` to pass additional arguments to your module files
+    #extraSpecialArgs = {
+    #};
+  };
+  nvim = nixvim'.makeNixvimWithModule nixvimModule;
 in
 with lib;
 {
   config = (mkIf cfg.enable {
-    programs.nixvim = mkMerge [
-      # Global config
-      {
-        enable = true;
-        extraConfigVim = ''
-        let mapleader=" "
-        nnoremap <SPACE> <Nop>
-
-        " Show line number
-        set number
-        " Relative number
-        set relativenumber
-        '';
-      }
-
-      # Which-key config
-      {
-        plugins.which-key= {
-          enable = true; 
-        };
-      }
-
-      # Telescope config
-      {
-        plugins.telescope = {
-          enable = true;
-          extensions = {
-            file-browser = {
-              enable = true;
-              settings.hidden = { file_browser = false; folder_browser = true; };
-            };
-            media-files = { enable = true; };
-            ui-select.enable = true;
-            undo.enable = true;
-          };
-        };
-        plugins.which-key.registrations."<leader>f" = "Telescope find";
-        keymaps = [
-          {
-            key = "<leader>ff";
-            action = "<cmd>Telescope find_files<cr>";
-            option = {
-              silent = false;
-              desc = "Find files ";
-            };
-          }
-          {
-            key = "<leader>fg";
-            action = "<cmd>Telescope live_grep<cr>";
-            option = {
-              silent = false;
-              desc = "Live Grep";
-            };
-          }
-          {
-            key = "<leader>fb";
-            action = "<cmd>Telescope buffers<cr>";
-            option = {
-              silent = false;
-              desc = "Telescope buffers";
-            };
-          }
-          {
-            key = "<leader>fh";
-            action = "<cmd>Telescope help_tags<cr>";
-            option = {
-              silent = false;
-              desc = "Telescope help_tags";
-            };
-          }
-        ];
-      }
-    ]; 
+    #environment.systemPackages = [nvim];
   });
 }
